b537ad991d6788bd6a6ce8f8d626df12246ee2e8
admin <admin@AR-114-02>
2018-05-22 13:41:10 +0200

    Første GIT project

---
+++ TestAppConsole/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace TestAppConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ínput = "Hello World";
+            Console.WriteLine(ínput);
+            Console.WriteLine("Press any key to write to file...");
+            string inputcmd;
+            inputcmd = Console.ReadLine();
+            File.AppendAllText("c:\\temp\\minfil.txt", inputcmd + Environment.NewLine);
+
+            //Console.WriteLine("Data written to file");
+            string[] readtext = File.ReadAllLines("c:\\temp\\minfil.txt");
+
+            foreach (string s in readtext)
+            { Console.WriteLine(s); }
+            Console.ReadLine();
+        }
+    }
+}
