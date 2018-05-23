ff6fc812fde78685c10c361d8a792acd485ef6b7
admin <admin@AR-114-02>
2018-05-23 12:07:06 +0200

    Øvelse 4

---
+++ String/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            char t = 'A';
+
+
+
+                string navn = "Pelle Gris";
+
+            Console.WriteLine(navn);
+
+            //Man kan ikke ændre i en tekststreng direkte
+            navn = navn.Replace('e', 'y');
+            Console.WriteLine(navn);
+
+            
+
+            //Split en fil
+            string csv="324;367;678;767";
+
+            string[] aa = csv.Split(';');
+
+            int alder = 10;
+            string navn2 = "Pelle Gris";
+
+            //Fra C# 6 kan man skrive tekst således
+            string s = $"{navn2} er {alder} år gammel";
+
+            Console.WriteLine(s);
+
+            //Stop ur
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            sw.Start();
+            sw.Stop();
+
+
+            Console.WriteLine(sw.Elapsed);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Øvelse4/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Øvelse4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fornavn = "Mikkel";
+            string efternavn = "Cronberg";
+
+            string samletnavn = fornavn + " " + efternavn;
+
+            string navnStort = samletnavn.ToUpper();
+
+            string navnLille = samletnavn.ToLower();
+
+            string del = samletnavn.Substring(7, 4);
+            Console.WriteLine("**************");
+
+            //Find det automatisk
+            string del2 = samletnavn.Substring(samletnavn.IndexOf("Cron"),4);
+            Console.WriteLine(del2);
+
+            string[] aa = samletnavn.Split(' ');
+
+            for (int i = 0; i < aa.Length; i++)
+            {
+                Console.WriteLine(aa[i]);
+            }
+
+            Console.WriteLine("Press any key!");
+            Console.ReadKey();
+
+        }
+    }
+}
