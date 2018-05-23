e0a256825d2426fecdafa23b1001fb62a47555a2
admin <admin@AR-114-02>
2018-05-23 10:36:14 +0200

    Arrays

---
+++ RefVariabler/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefVariabler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Arrays
+            //De er tomme
+            int[] myIntArray;
+            int[,] myIntArray2;
+            myIntArray = new int[5];
+
+            //multi dimensionel array
+            myIntArray2 = new int[5, 2];
+
+            //Lav en array med 9 værdier
+            int[] myInt3Array = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            Console.WriteLine(myInt3Array[0].ToString());
+
+            //Giv en ting i array en værdi
+            myInt3Array[0] = 10;
+            Console.WriteLine(myInt3Array[0].ToString());
+
+
+            //Giver fejl da array er defineret til 9
+            //myInt3Array[10] = 1;
+
+            Console.WriteLine("*****************");
+
+            //Loop igennem alle 
+            foreach (int i in myInt3Array)
+            {
+                Console.WriteLine("{0} ",i);
+            }
+
+
+            Console.WriteLine("********SORTERET *********");
+
+            //Sorter array
+            Array.Sort(myInt3Array);
+            foreach (int i in myInt3Array)
+            {
+                Console.WriteLine("{0} ", i);
+            }
+
+
+
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
