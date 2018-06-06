470f69ec87119d0f9a60b8fef3c66e2f7f4c6992
Anders Sørensen <anders@galberg.com>
2018-06-06 19:54:17 +0200

    StatiskeMetoder

---
+++ StatiskeMetoder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StatiskeMetoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(ArealBeregninger.BeregnArealFirkant(10,10));
+            Console.WriteLine(ArealBeregninger.BeregnArealCirkel(10));
+
+            Console.ReadKey();
+        }
+    }
+
+    class ArealBeregninger
+    {
+
+
+        public static double BeregnArealFirkant (double højde, double brede)
+        {
+
+            return højde * brede;
+        }
+
+        public static double BeregnArealCirkel(double radius)
+        {
+            return Math.Pow(radius, 2) * System.Math.PI;
+
+        }
+
+
+    }
+}
