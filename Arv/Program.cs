1514e478361efcaea9d742efa095655be7a0ed24
admin <admin@AR-114-02>
2018-06-07 10:57:39 +0200

    Arv x 2

---
+++ Arv/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Elev p1 = new Elev();
+
+            p1.Fornavn = "Anders";
+            p1.Efternavn = "Sørensen";
+            p1.Klasselokale "Til højre"
+            
+
+            
+
+            Console.WriteLine(p1.FuldtNavn());
+
+        }
+    }
+
+    public class Person
+    {
+
+        public string Fornavn { get; set; }
+        public string Efternavn { get; set; }
+
+        public string FuldtNavn()
+        {
+            return Fornavn + " " + Efternavn;
+        }
+    }
+
+    public class Elev : Person
+    {
+        public string Klasselokale { get; set; }
+
+
+    }
+
+    class Instruktør : Person
+    {
+        public int NøgleID { get; set; }
+    }
+}
---
+++ Arv_random/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arv_random
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            UdvidetRandom r = new UdvidetRandom();
+            Console.WriteLine(r.NextBool());
+            Console.WriteLine(r.NextBool2());
+
+        }
+    }
+
+    class UdvidetRandom : System.Random
+    {
+
+        public bool NextBool() //Min løsning
+        {
+            Random rnd = new System.Random();
+
+            int _værdi = rnd.Next(1, 1002);
+
+            if (_værdi <= 500)
+            {
+                return true;
+            }
+            else
+            {
+                return false;    
+            }
+
+
+        }
+
+        public bool NextBool2() //Løsning fra opgaven
+        {
+            return this.Next(1, 1002) < 500;
+        }
+
+    }
+}
