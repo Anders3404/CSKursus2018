bfee9e4fd387c79948514341474cb1cd04b36799
admin <admin@AR-114-02>
2018-06-20 14:27:21 +0200

    FileWatch + Lambda

---
+++ FileWatch/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FileCheck();
+
+            do
+            {
+
+            } while (true);
+        }
+
+        private static void FileCheck()
+        {
+            FileSystemWatcher w = new FileSystemWatcher(@"c:\temp", "*.txt");
+            w.EnableRaisingEvents = true;
+            w.Changed += W_Changed;
+            w.Created += (s, e) => { Console.WriteLine("Oprettet " + e.FullPath); };
+            w.Deleted += (s, e) => { Console.WriteLine("Filen er slettet.  " + e.FullPath); };
+        }
+
+        private static void W_Changed(object sender, FileSystemEventArgs e)
+        {
+            Console.WriteLine("Tekst er tilføjet: " + e.Name);
+        }
+    }
+}
---
+++ Lambda/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lambda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> a = new List<int>() { 5, 1, 51, 7, 1, 56, 36, 5 };
+            Console.WriteLine(a.FindIndex(FindVærdi));
+            Console.WriteLine(   a.FindIndex( x => x==51));
+            Console.WriteLine("***");
+            a.ForEach(ListVærdi);
+            Console.WriteLine("***");
+            a.ForEach(i => { Console.WriteLine(i); });
+            //Console.WriteLine("***");
+
+        }
+
+        static bool FindVærdi(int i)
+        {
+            return i == 51;
+        }
+
+        static void ListVærdi(int i)
+        { 
+
+        Console.WriteLine(i);
+        }
+    }
+}
